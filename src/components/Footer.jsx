94f436103bfafc39c3992fd0a84bd2f7c1566ea7
TaraRhoseyn <taragulwell@gmail.com>
2025-01-24 11:16:36 +0000

    add Welsh translation to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 
 const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="max-w-screen-xl mx-auto px-4">
-        <div className="text-center">
-          <p>© {new Date().getFullYear()} Sport Wales Digital. All rights reserved.</p>
-        </div>
-      </div>
-    </footer>
-  );
+	const isWelsh = useLocation().pathname.includes('/cy');
+
+	const footerContent = () => {
+		if (isWelsh) {
+			return ' Chwaraeon Cymru. Cedwir pob hawl.';
+		} else {
+			return ' Sport Wales Digital. All rights reserved.';
+		}
+	};
+
+	return (
+		<footer className="bg-gray-800 text-white py-8">
+			<div className="max-w-screen-xl mx-auto px-4">
+				<div className="text-center">
+					<p>© {new Date().getFullYear()} {footerContent()}</p>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
